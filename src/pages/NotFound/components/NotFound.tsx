587c5f5c02f5ebfe51c3a0d4e81b040cb9d814df
utkinahata <Amihajlov580@gmail.com>
2023-01-25 16:38:33 +0300

    finished not-found page

--- src/pages/NotFound/components/NotFound.tsx
+++ src/pages/NotFound/components/NotFound.tsx
@@ -4,12 +4,16 @@ import "./NotFound.css";
 const NotFound = () => {
     return(
         <div className="not_found_background">
+            <div className="not_found_text">
             <div className="not_found_description">
                 Oops,the page you are looking for get lost.
+                
             </div>
             <Link to="/">
                 <button className="not_found_button">Go back home</button>
             </Link>
+            </div>
+            <p className="copyright"> Innowise 2023 | Andrew Mihaylov</p>
         </div>
     )
 }
